2e670cef271839e6ba3f4722d95f9fcae2b79385
miyabihito <miyabihito.dev@gmail.com>
2014-06-02 02:00:08 +0900

    use should.js for test

--- test/lib/visitor.js
+++ test/lib/visitor.js
@@ -1,19 +1,18 @@
 /*
  * Module dependencies
  */
-var assert = require('assert');
 var Visitor = require('../../lib/visitor');
 
 describe('Visitor', function() {
 	it('should have id', function() {
 		var id = 'id';
 		var visitor = new Visitor(id);
-		assert.equal(visitor.id, id);
+		visitor.should.have.property('id', id);
 	});
 
 	it('should have random color', function() {
 		var visitor = new Visitor('id');
 		visitor.randomizeColor();
-		assert.ok(/^#[0-9a-f]{6}$/.test(visitor.color));
+		visitor.should.have.property('color').which.match(/^#[0-9a-f]{6}$/);
 	});
 });
